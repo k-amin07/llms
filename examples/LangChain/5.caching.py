367d438e9da038e7155e7a1d1c9815829c0ba1fb
Khizar Amin <khizaramin95@gmail.com>
2024-07-31 16:33:24 +0500

    vllm batched inference

--- examples/LangChain/5.caching.py
+++ examples/LangChain/5.caching.py
@@ -5,12 +5,12 @@ from langchain_community.cache import InMemoryCache
 
 
 llm = ChatOpenAI(model_name="gpt-4o-mini", n=2)
-# set_llm_cache(InMemoryCache())
+set_llm_cache(InMemoryCache())
 
 # We can do the same thing with a SQLite cache
-from langchain.cache import SQLiteCache
+# from langchain.cache import SQLiteCache
 
-set_llm_cache(SQLiteCache(database_path=".langchain.db"))
+# set_llm_cache(SQLiteCache(database_path=".langchain.db"))
 
 from datetime import datetime
 startTime = datetime.now()
